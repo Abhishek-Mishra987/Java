da6fef57a9f9dc2b3327eace5bc9d637d7c01f88
Abhishek Mishra <abhishekmishraa987@gmail.com>
2026-01-10 15:02:27 +0530

    sort zero and one

---
+++ NewJava/SortOneandZero.java
@@ -0,0 +1,26 @@
+package NewJava;
+
+import java.util.Arrays;
+
+public class SortOneandZero {
+    public static void oneZeroSort(int[] arr) {
+        int left = 0, right = arr.length-1;
+        int temp;
+        while(left<right){
+            if (arr[left] == 1) {
+                if(arr[right] != 1){
+                    temp=arr[left];
+                    arr[left]=arr[right];
+                    arr[right]=temp;
+                }
+                right--;
+            }else
+                left++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {0,1,1,0,1,0,1};
+        oneZeroSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
